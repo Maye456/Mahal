936055a641379c010fa23f427fef2376473e416e
JeannaMaye <54680474+Maye456@users.noreply.github.com>
2022-07-25 17:50:37 -0700

    Entire Project Upload
    Initial Project Upload

---
+++ Mahal/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Mahal.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Mahal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Photos()
+        {
+            return View();
+        }
+
+        public IActionResult Videos()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mahal/Views/Home/Photos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mahal.Views.Home
+{
+    public class PhotosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
