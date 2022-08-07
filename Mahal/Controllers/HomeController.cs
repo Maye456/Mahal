0a0f5ff8ce99288c0a5c7136a5dde40263301924
JeannaMaye <54680474+Maye456@users.noreply.github.com>
2022-08-06 17:12:44 -0700

    Final Push
    Final

--- Mahal/Controllers/HomeController.cs
+++ Mahal/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace Mahal.Controllers
             return View();
         }
 
+        public IActionResult Cards()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
