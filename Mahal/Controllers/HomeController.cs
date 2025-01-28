5360e1ce228c4aa2099a4d84f79cb5c1d4655145
JeannaMaye <54680474+Maye456@users.noreply.github.com>
2025-01-27 23:15:48 -0800

    New update 01-27-2025
    New update 01-27-2025

--- Mahal/Controllers/HomeController.cs
+++ Mahal/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace Mahal.Controllers
             return View();
         }
 
+        public IActionResult Valentine()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
